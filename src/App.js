969428c4a8ac0ba4b3c172e4bc56c7ae234b4587
Preet Patel <preetpatel9121@gmail.com>
2023-07-18 22:08:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import axios from 'axios';
+import Weather from './Weather';
+function App(){
+  return (
+    <div>
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
